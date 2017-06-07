72291958fd5ee9ece763b1fd5945f5f53dd7dba8
martysun <martian206@gmail.com>
2017-06-07 19:52:55 +0800

    init commit

--- src/index.jsx
+++ src/index.jsx
@@ -5,35 +5,13 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      greetingMessage: 'Your name?',
-      user: ''
-    }
-  }
-
-  onChange = (e) => {
-    this.setState({user: e.target.value})
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    if (this.state.user !== '') {
-      this.setState({
-        greetingMessage: 'Hello, '+this.state.user+'!',
-        user: ''
-      })
-    } else {
-      this.setState({greetingMessage: 'No name?'})
     }
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.greetingMessage}</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input onChange={this.onChange} placeholder="your name here" value={this.state.user}/>
-          <button>SEND</button>
-        </form>
+        <h1>Hello World!</h1>
       </div>
     )
   }
